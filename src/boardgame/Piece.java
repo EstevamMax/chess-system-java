43e7da0ac92b5cec8a2c25971f04f356e656722a
Estevam Max <estevamms98@outlook.com>
2021-05-03 20:52:47 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position;
 	private Board board;
@@ -12,5 +12,23 @@ public class Piece {
 	
 	protected Board getBoard() {
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() { //Método para percorrer a matriz de movimentos e verificar se a algum possivel movimento para a peça
+		boolean [][] mat = possibleMoves(); //Matriz auxiliar para percorrer a matriz de posições
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j < mat.length; j++) {
+				if(mat[i][j]) { //Retorna verdadeiro caso hajá algum movimento possivel da peça na matriz
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
